e487b9caaad0d44837bf4cad014367c057afb5f6
regestea23 <amirhossein5starstar@outlook.com>
2025-08-16 13:34:11 +0330

    fix card styles

--- src/components/DownloadFile.tsx
+++ src/components/DownloadFile.tsx
@@ -9,22 +9,20 @@ type DownloadFileProps = {
 }
 
 function DownloadFile(props: DownloadFileProps) {
-
-    console.log("render download file");
-    console.log(props);
-
+    
     return <>
 
-        <div className="form-step items-center justify-center">
+        <div className=" mt-24 justify-center">
 
-            <div id="result-container" className=" w-full max-w-sm">
+            <div className=" w-full ">
                 {DownloadCard(props)}
                 
-                <button
-                    onClick={props.onDoneClick}
-                    className="w-full mt-6 bg-cyan-500 text-white font-bold rounded-lg py-3 hover:bg-cyan-600 transition-colors">Done
-                </button>
+                
             </div>
+            <button
+                onClick={props.onDoneClick}
+                className="w-full mt-36 bg-cyan-500 text-white font-bold rounded-lg py-3 hover:bg-cyan-600 transition-colors">Done
+            </button>
         </div>
     </>;
 }
@@ -33,36 +31,42 @@ function DownloadCard(props: DownloadFileProps) {
     switch (props.fileFormat) {
         case FileFormats.PDF:
             return <>
-                <div className="result-card  glass rounded-2xl p-8 text-center animate-fade-in">
-                    <FontAwesomeIcon icon={faFilePdf} className={"text-7xl text-red-400"}/>
-                    <p className="mt-4 font-bold text-xl">Your PDF is ready!</p>
+                <div className="glass rounded-2xl p-8 flex flex-col items-center gap-4 text-center">
+                    <FontAwesomeIcon icon={faFilePdf} className="text-7xl text-red-400" />
+
                     <a
                         href={props.fileUrl}
                         download={"pamphlet.pdf"}
-                        className="w-full mt-6 bg-red-400 text-white font-bold rounded-lg py-3 hover:bg-red-500 transition-colors">Download
+                        className="w-full p-4 bg-red-400 text-white font-bold rounded-lg hover:bg-red-500 transition-colors"
+                    >
+                        Download
                     </a>
                 </div>
+
             </>
         case FileFormats.Word:
             return <>
-                <div className="result-card  glass rounded-2xl p-8 text-center animate-fade-in">
+
+                <div className="glass rounded-2xl p-8 flex flex-col items-center gap-4 text-center">
                     <FontAwesomeIcon icon={faFileWord} className={"text-7xl text-blue-500"}/>
-                    <p className="mt-4 font-bold text-xl">Your DOCX file is ready!</p>
                     <a
                         href={props.fileUrl}
                         download={"pamphlet.docx"}
-                        className="w-full mt-6 bg-blue-500 text-white font-bold rounded-lg py-3 hover:bg-blue-600 transition-colors">Download
+                        className="w-full p-4 bg-blue-500 text-white font-bold rounded-lg hover:bg-blue-600 transition-colors"
+                    >
+                        Download
                     </a>
                 </div>
+                
             </>
         case FileFormats.Markdown:
             return <>
-                <div className="result-card  glass rounded-2xl p-8 text-center animate-fade-in">
+                <div className="flex flex-col glass rounded-2xl p-8 text-center ">
                     <div
                         className="w-20 h-20 mx-auto rounded-lg bg-gradient-to-br from-blue-400 to-purple-500 flex items-center justify-center">
                         <span className="text-white font-black text-4xl">MD</span>
                     </div>
-                    <p className="mt-4 font-bold text-xl">Your Markdown file is ready!</p>
+                    
                     <a
                         href={props.fileUrl}
                         download={"pamphlet.md"}
